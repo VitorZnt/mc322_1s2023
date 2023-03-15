9f922b7ee52ac0128acc729fb10e56fc6edc58fe
Vitor Rodrigues Zanata da Silva <v231718@dac.unicamp.br>
2023-03-14 22:54:22 -0300

    comeca a estrutura basica das classes Seguradora e Cliente

--- Seguradora/src/Seguradora.java
+++ Seguradora/src/Seguradora.java
@@ -1,5 +1,50 @@
 public class Seguradora {
-	public static void main(String args[]) {
+	// Atributos de instancia
+	private String nome;
+	private String telefone;
+	private String email;
+	private String endereco;
+	
+	// Construtor
+	public Seguradora(String nome, String telefone, String email, String endereco) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+		this.endereco = endereco;
+	}
+	
+	// Metodos de Get e Set
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void set(String email) {
+		this.email = email;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	// Metodo main
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
+	
 }
