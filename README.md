91833cd7fecb0b1d465a7c1c5a6ecad5b1d064f3
Vitor Rodrigues Zanata da Silva <v231718@dac.unicamp.br>
2023-03-28 22:20:12 -0300

    Atualiza Readme e corrige identacoes

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ## Sobre a estrutura dos arquivos:
 
 A estrutura foi criada pelo Eclipse no modo "Novo projeto". Os arquivos JAVA estao na pasta "scr", como exigido. No entanto, o Eclipse nao inclui a pasta "lib" e nem "bin" ao pedir os comandos "git add ." e "git pull". Isso foi descrito ao professor e aprovado pelo mesmo na aula de lab no dia 21/03/2023.
+
+## Indentacao no Lab2:
+
+No Lab2, Eclipse estava misturando Tabs e espacos para indentacao e os documentos no Github ficaram com identacao estranha em alguns pontos. Isso foi reconfigurado corrigido nos outros labs posteriores.
--- Seguradora/src/Cliente.java
+++ Seguradora/src/Cliente.java
@@ -1,118 +1,118 @@
 public class Cliente {
-	// Atributos de instancia
-	private String nome;
-	private String cpf;
-	private String dataNascimento;
-	private int idade;
-	private String endereco;
-	
-	// Construtor
-	public Cliente (String nome, String cpf, String dataNascimento, int idade, String endereco) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.dataNascimento = dataNascimento;
-		this.idade = idade;
-		this.endereco = endereco;
-	}
-	
-	// Metodos de Get e Set
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	
-	// Metodos especificos
-	public String toString() {
-		String str = String.format("Nome: %s\nCpf: %s\nData de nascimento: %s\nIdade: %d\nEndereco: %s\n",
-				nome, cpf, dataNascimento, idade, endereco);
-		return str;
-	}
-	
-	/* Recebe uma string numerica de tamanho 11. Verifica se o cpf eh
-	 * composto de 11 digitos iguais e retorna true caso sim.*/
-	private boolean verificarDigitosIguais(String cpf) {
-		boolean flag = true;
-		int i = 0;
-		while (flag && i < 10) {
-			if (cpf.charAt(i) - '0' != cpf.charAt(i+1) - '0')
-				flag = false;
-			i++;
-		}
-		return flag;
-	}
-	
-	/* Recebe um cpf em forma de string e checa se seus digitos verificadores
-	 * sao correspondentes. Caso seja um cpf valido, retorna true.*/
-	private boolean verificarDigitos(String cpf) {
-		int soma = 0;
-		int digito1, digito2;
+    // Atributos de instancia
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private int idade;
+    private String endereco;
 
-		// Calculo do digito1
-		for (int i = 0; i < 9; i++)
-			soma += (cpf.charAt(i) - '0') * (10-i);
-		soma = soma % 11;
-		if (11 - soma < 10)
-			digito1 = 11 - soma;
-		else
-			digito1 = 0;
+    // Construtor
+    public Cliente (String nome, String cpf, String dataNascimento, int idade, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
 
-		// Calculo do digito2
-		soma = 0;
-		for (int i = 0; i < 9; i++)
-			soma += (cpf.charAt(i) - '0') * (11-i);
-		soma += digito1 * 2;
-		soma = soma % 11;
-		if (11 - soma < 10)
-			digito2 = 11 - soma;
-		else
-			digito2 = 0;
-		
-		// Comparacao dos digitos esperados e dos recebidos
-		if (digito1 == cpf.charAt(9) - '0' && digito2 == cpf.charAt(10) - '0')
-			return true;
-		else
-			return false;
-	}
-	
-	public boolean validarCPF(String cpf) {
-		cpf = cpf.replaceAll("\\.", "");
-		cpf = cpf.replaceAll("-", "");
-		// Verifica o tamanho
-		if (cpf.length() != 11 || verificarDigitosIguais(cpf)) {
-			return false;
-		} else {
-			return verificarDigitos(cpf);
-		}
-	}
-	
+    // Metodos de Get e Set
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    // Metodos especificos
+    public String toString() {
+        String str = String.format("Nome: %s\nCpf: %s\nData de nascimento: %s\nIdade: %d\nEndereco: %s\n",
+                nome, cpf, dataNascimento, idade, endereco);
+        return str;
+    }
+
+    /* Recebe uma string numerica de tamanho 11. Verifica se o cpf eh
+     * composto de 11 digitos iguais e retorna true caso sim.*/
+    private boolean verificarDigitosIguais(String cpf) {
+        boolean flag = true;
+        int i = 0;
+        while (flag && i < 10) {
+            if (cpf.charAt(i) - '0' != cpf.charAt(i+1) - '0')
+                flag = false;
+            i++;
+        }
+        return flag;
+    }
+
+    /* Recebe um cpf em forma de string e checa se seus digitos verificadores
+     * sao correspondentes. Caso seja um cpf valido, retorna true.*/
+    private boolean verificarDigitos(String cpf) {
+        int soma = 0;
+        int digito1, digito2;
+
+        // Calculo do digito1
+        for (int i = 0; i < 9; i++)
+            soma += (cpf.charAt(i) - '0') * (10-i);
+        soma = soma % 11;
+        if (11 - soma < 10)
+            digito1 = 11 - soma;
+        else
+            digito1 = 0;
+
+        // Calculo do digito2
+        soma = 0;
+        for (int i = 0; i < 9; i++)
+            soma += (cpf.charAt(i) - '0') * (11-i);
+        soma += digito1 * 2;
+        soma = soma % 11;
+        if (11 - soma < 10)
+            digito2 = 11 - soma;
+        else
+            digito2 = 0;
+
+        // Comparacao dos digitos esperados e dos recebidos
+        if (digito1 == cpf.charAt(9) - '0' && digito2 == cpf.charAt(10) - '0')
+            return true;
+        else
+            return false;
+    }
+
+    public boolean validarCPF(String cpf) {
+        cpf = cpf.replaceAll("\\.", "");
+        cpf = cpf.replaceAll("-", "");
+        // Verifica o tamanho
+        if (cpf.length() != 11 || verificarDigitosIguais(cpf)) {
+            return false;
+        } else {
+            return verificarDigitos(cpf);
+        }
+    }
+
 }
