ca1e690afa531f46876fdffb88de4d0953890ec3
Vitor Rodrigues Zanata da Silva <v231718@dac.unicamp.br>
2023-04-13 15:02:56 -0300

    adiciona e implementa listaVeiculos no Cliente e seus metodos

--- Lab03/src/Cliente.java
+++ Lab03/src/Cliente.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class Cliente {
     // Atributos de instancia
     private String nome;
     private String endereco;
-
+    private ArrayList<Veiculo> listaVeiculos;
 
     //IMPLEMENTAR LISTA CARROS
 
@@ -10,6 +12,7 @@ public class Cliente {
     public Cliente(String nome, String endereco) {
         this.nome = nome;
         this.endereco = endereco;
+        listaVeiculos = new ArrayList<Veiculo>();
     }
 
     // Metodos de Get e Set
@@ -27,6 +30,38 @@ public class Cliente {
         this.endereco = endereco;
     }
 
+    
+    //Recebe os dados de um veiculo a ser criado e o adiciona a um cliente.
+    public void cadastrarVeiculo(String placa, String marca, String modelo, int anoFabricacao) {
+        Veiculo novo = new Veiculo(placa, marca, modelo, anoFabricacao);
+        listaVeiculos.add(novo);
+        return;
+    }
+    
+    
+    /*Busca um veiculo de placa fornecida. Se existir, retorna sua posicao na ArrayList.
+     * Caso contrario, retorna -1 
+     */
+    public int buscarVeiculo(String placa) {
+        Veiculo veic;
+        for (int i = 0; i < listaVeiculos.size(); i++) {
+            veic = listaVeiculos.get(i);
+            if (veic.getPlaca() == placa)
+                return i;
+        }
+        return -1;
+    }
+    
+    //Tenta remover um veiculo de dada placa. Retorna true caso bem sucedido.
+    public boolean removerVeiculo(String placa) {
+        int i = buscarVeiculo(placa);
+        if (i == -1)
+            return false;
+        else {
+            listaVeiculos.remove(i);
+            return true;
+        }
+    }
 
 
 
